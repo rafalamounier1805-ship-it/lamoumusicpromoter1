46b54cfa362b3495a18aba3dbdbda37458f4c566
rafalamounier1805-ship-it <rafalamounier1805@gmail.com>
2026-08-15 04:55:41 -0300

    refresh app cache after creative flow fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lamou-spotify-workflow-v2';
+const CACHE='lamou-spotify-workflow-v3';
 const ASSETS=['./','./index.html','./styles.css','./workflow.css','./shell-v10.js','./app.js','./integrations-v10.js','./manifest.webmanifest','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
