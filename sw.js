af03e9d7e535497631255d7eb0ce8e068f2dac3c
rafalamounier1805-ship-it <rafalamounier1805@gmail.com>
2026-08-19 21:03:56 -0300

    LAMOU v15.2.1: renovar cache do app

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE='lamou-runtime-v152';
+const CACHE='lamou-runtime-v1521';
 const ASSETS=[
   './','./index.html',
-  './styles.css?v=152','./submissions-v15.css?v=152',
-  './submissions-standalone-v152.js?v=152',
-  './manifest.webmanifest?v=152','./icon-192.svg','./icon-512.svg'
+  './styles.css?v=1521','./submissions-v15.css?v=1521',
+  './submissions-standalone-v152.js?v=1521',
+  './manifest.webmanifest?v=1521','./icon-192.svg','./icon-512.svg'
 ];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
