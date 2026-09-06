656ef4331bd9f9f2742f1deb6cb1891362d73eb2
rafalamounier1805-ship-it <rafalamounier1805@gmail.com>
2026-09-06 09:53:35 -0300

    feat: add LAMOU client portal PWA v0.2

---
+++ suite/client-portal/config.js
@@ -0,0 +1,6 @@
+window.LAMOU_CONFIG = Object.freeze({
+  supabaseUrl: "https://afzdqrryqsuwpkaiinez.supabase.co",
+  publishableKey: "sb_publishable_Qmx6sPa323jSAiMEdCkjvw_Y0MakWka",
+  refreshMs: 30000,
+  offlineStaleMs: 10 * 60 * 1000
+});
