d7a5e4525fa2f7bf016147efa85e5f631164574c
rafalamounier1805-ship-it <rafalamounier1805@gmail.com>
2026-09-23 23:56:00 -0300

    fix(B144): remove duplicate one-click updater import

--- owner-console/src/routes/owner.versions.tsx
+++ owner-console/src/routes/owner.versions.tsx
@@ -1,7 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 import { AppShell } from "@/components/lamou/app-shell";
-import { OneClickUpdate } from "@/components/lamou/one-click-update";
 import { ModulePage } from "@/components/lamou/module-page";
 import { OneClickUpdate } from "@/components/lamou/one-click-update";
 import { Panel, TruthBadge } from "@/components/lamou/shell";
